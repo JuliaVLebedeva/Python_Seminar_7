158028af0d3e784c09f0ad5886a115f3d51bd779
Юлия Лебедева <Lebedeva@mail.ru>
2022-11-18 13:54:36 +0300

    модуль журнал решений

---
+++ log.py
@@ -0,0 +1,9 @@
+
+def logger(primer, result):
+
+    with open('file.txt', 'a') as data:
+        rasparse = ("".join(map(str, primer)))
+        rasparse = rasparse + " = "
+        result = str(result) + "; \n"
+        data.write(str(rasparse))
+        data.write(str(result))
